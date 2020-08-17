fd68f497c145ae5e70dd7e979d47c09337e4fb9b
GhadeerAbuoda <3702585+GhadeerAbuoda@users.noreply.github.com>
2020-08-17 13:30:02 +0300

    Update Q1-RDFframes.py

--- benchmark_queries/Q1-RDFframes.py
+++ benchmark_queries/Q1-RDFframes.py
@@ -1,7 +1,5 @@
 ''' 
 Get the nationality, place, date of birth for each basketball player, in addition to the sponsor, name, and president of his team (if available).
-from time import time
-
 '''
 
 from time import time
