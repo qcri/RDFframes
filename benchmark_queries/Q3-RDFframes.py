4c5864b15f3a49bfb090fc74903e5d80d7aa5fbe
GhadeerAbuoda <3702585+GhadeerAbuoda@users.noreply.github.com>
2020-08-17 13:36:35 +0300

    Update Q3-RDFframes.py

--- benchmark_queries/Q3-RDFframes.py
+++ benchmark_queries/Q3-RDFframes.py
@@ -1,3 +1,5 @@
+''' Get the sponsor, name, president, and the number of basketball players (if available) for each basketball team. '''
+
 from rdfframes.knowledge_graph import KnowledgeGraph
 from rdfframes.utils.constants import JoinType
 
