45a46aedeeeae9a054efb27ed2007a43cd521348
GhadeerAbuoda <3702585+GhadeerAbuoda@users.noreply.github.com>
2020-08-17 13:44:17 +0300

    Update Q13-RDFframes.py

--- benchmark_queries/Q13-RDFframes.py
+++ benchmark_queries/Q13-RDFframes.py
@@ -1,4 +1,4 @@
-'''    Q13 & Get a list of films in DBpedia. For each film, return the actor, language,
+'''  Q13 & Get a list of films in DBpedia. For each film, return the actor, language,
 country, genre, story, studio in addition to the director, producer, and title (if available). '''
 
 from rdfframes.knowledge_graph import KnowledgeGraph
