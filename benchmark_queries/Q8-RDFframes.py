963d24dd1a03de91d7bb8d3d4a01dfa4cd640780
GhadeerAbuoda <3702585+GhadeerAbuoda@users.noreply.github.com>
2020-08-17 13:40:23 +0300

    Update Q8-RDFframes.py

--- benchmark_queries/Q8-RDFframes.py
+++ benchmark_queries/Q8-RDFframes.py
@@ -1,3 +1,8 @@
+
+''' Dataset1: Info. about the Films in DBpedia: actor, director, country, producer,
+   language, title, genre, story, studio . Filter on country, studio and genre, runtime. '''
+
+
 from rdfframes.knowledge_graph import KnowledgeGraph
 
 
