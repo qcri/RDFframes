7177765163fa67c76954126908a71e617e8cddb8
GhadeerAbuoda <3702585+GhadeerAbuoda@users.noreply.github.com>
2021-03-07 11:07:21 +0300

    Create Q1-RDFframes.py

---
+++ benchmark_DBpedia2020/Q1-RDFframes.py
@@ -0,0 +1,23 @@
+''' 
+Get the nationality, place, date of birth for each basketball player, in addition to the sponsor, name, and president of his team (if available).
+'''
+
+from time import time
+from rdfframes.knowledge_graph import KnowledgeGraph
+from rdfframes.utils.constants import JoinType
+from rdfframes.client.http_client import HttpClientDataFormat, HttpClient
+
+graph =  KnowledgeGraph(graph_name='dbpedia')
+
+
+def expand_join(join_type):
+    basketball_palyer = graph.entities('dbpo:BasketballPlayer', entities_col_name='player')\
+      .expand('player', [('dbpp:nationality', 'nationality') ,('dbpp:birthPlace', 'place'),
+                         ('dbpp:birthDate','birthDate'),('dbpp:team', 'team')])
+    basketball_team = graph.entities('dbpo:BasketballTeam', entities_col_name='team')\
+      .expand('team', [('dbpp:president', 'president'), ('dbpp:sponsor', 'sponsor'), ('dbpp:name', 'name')])
+    basketball_palyer_team = basketball_palyer.join(basketball_team,'team', join_type=join_type)
+    print(basketball_palyer_team.to_sparql())
+
+
+expand_join(JoinType.LeftOuterJoin)
