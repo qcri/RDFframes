c9521bab6cc4bd921cdb5a77fdc52df9dbd237a8
GhadeerAbuoda <3702585+GhadeerAbuoda@users.noreply.github.com>
2021-03-08 12:05:38 +0300

    Update Q1-RDFframes.py

--- benchmark_DBpedia2020/Q1-RDFframes.py
+++ benchmark_DBpedia2020/Q1-RDFframes.py
@@ -1,23 +1,20 @@
-''' 
-Get the nationality, place, date of birth for each basketball player, in addition to the sponsor, name, and president of his team (if available).
-'''
+''' Get a list of films in DBpedia. For each film, return the actor, language,
+    country, genre, story, studio in addition to the director, producer, and title (if available). '''
 
-from time import time
 from rdfframes.knowledge_graph import KnowledgeGraph
-from rdfframes.utils.constants import JoinType
-from rdfframes.client.http_client import HttpClientDataFormat, HttpClient
-
-graph =  KnowledgeGraph(graph_name='dbpedia')
 
 
-def expand_join(join_type):
-    basketball_palyer = graph.entities('dbpo:BasketballPlayer', entities_col_name='player')\
-      .expand('player', [('dbpp:nationality', 'nationality') ,('dbpp:birthPlace', 'place'),
-                         ('dbpp:birthDate','birthDate'),('dbpp:team', 'team')])
-    basketball_team = graph.entities('dbpo:BasketballTeam', entities_col_name='team')\
-      .expand('team', [('dbpp:president', 'president'), ('dbpp:sponsor', 'sponsor'), ('dbpp:name', 'name')])
-    basketball_palyer_team = basketball_palyer.join(basketball_team,'team', join_type=join_type)
-    print(basketball_palyer_team.to_sparql())
+graph = KnowledgeGraph(graph_name='dbpedia')
 
 
-expand_join(JoinType.LeftOuterJoin)
+def expand_optional():
+    Films = graph.entities('dbpo:Film', entities_col_name='film')\
+        .expand('film', [('dbpp:starring', 'actor'), ('dbpp:director','director', True),
+                         ('dbpp:country', 'movie_country'), ('dbpp:producer', 'producer', True),
+                         ('dbpp:language', 'language'), ('dbpp:story','story'),
+                         ('dbpp:runtime', 'runtime'), ('dbpp:studio' ,'studio', True), ('dbpp:title', 'title'),
+                         ('dbpp:genre', 'genre')])
+    print("SPARQL Query = \n{}".format(Films.to_sparql()))
+
+
+expand_optional()
