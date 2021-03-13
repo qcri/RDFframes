58c44200ad5ab94bc4055b8680e7f6bb17c6c4f0
GhadeerAbuoda <3702585+GhadeerAbuoda@users.noreply.github.com>
2021-03-13 21:58:39 +0300

    Create Q14-RDFframes.py

---
+++ benchmark_DBpedia2020/Q14-RDFframes.py
@@ -0,0 +1,21 @@
+''' Get the sponsor, name, president, and the number of basketball players (if available) for each basketball team. '''
+
+from rdfframes.knowledge_graph import KnowledgeGraph
+from rdfframes.utils.constants import JoinType
+from rdfframes.client.http_client import HttpClientDataFormat, HttpClient
+from time import time
+
+graph = KnowledgeGraph(graph_name='dbpedia')
+
+def expand_groupby_join(join_type):
+    basketball_palyer = graph.entities('dbpo:BasketballPlayer', entities_col_name='player')\
+        .expand('player', [('dbpp:team', 'team')])\
+        .group_by(['team']).count('player', 'count_basketball_players', True)
+
+    basketball_team = graph.entities('dbpo:BasketballTeam', entities_col_name='team')\
+    .expand('team', [('dbpo:league', 'league'), ('dbpp:location', 'location'), ('dbpp:name', 'name')])
+    basketball_palyer_team = basketball_team.join(basketball_palyer,'team', join_type=join_type)
+    print(basketball_palyer_team.to_sparql())
+
+expand_groupby_join(JoinType.LeftOuterJoin)
+
