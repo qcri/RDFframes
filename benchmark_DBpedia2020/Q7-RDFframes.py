ded3c24ac294a552b67ede9b19ed8491b18ac82a
GhadeerAbuoda <3702585+GhadeerAbuoda@users.noreply.github.com>
2021-03-13 21:18:59 +0300

    Create Q7-RDFframes.py

---
+++ benchmark_DBpedia2020/Q7-RDFframes.py
@@ -0,0 +1,24 @@
+
+''' Get information about the Films in DBpedia: actor, director, country, producer,
+   language, title, genre, story, studio . Filter on country, studio and genre, runtime. '''
+
+
+from rdfframes.knowledge_graph import KnowledgeGraph
+
+graph = KnowledgeGraph(graph_name='dbpedia')
+
+
+def expand_filter_expand():
+    films = graph.entities('dbpo:Film', entities_col_name='film')\
+        .expand('film', [('dbpp:starring', 'actor'), ('dbpp:country', 'movie_country')])\
+        .expand('film', [ ('dbpp:genre', 'genre')])\
+        .expand('film', [ ('dbpp:director','director'), ('dbpp:producer', 'producer'), ('dbpp:language', 'language'),
+                          ('dbpp:story','story') ,('dbpp:runtime', 'runtime'), ('dbpp:studio' ,'studio'),
+                          ('dbpp:title', 'title')])\
+        .filter({'movie_country': [' IN (dbpr:United_States, dbpr:India)']})\
+        .filter({'genre': ['IN (dbpr:Film_score, dbpr:Soundtrack, dbpr:Rock_music, dbpr:House_music, dbpr:Dubstep)']})\
+        .filter({'studio': ['!= "Eskay Movies"']})
+    print(films.to_sparql())
+
+
+expand_filter_expand()
